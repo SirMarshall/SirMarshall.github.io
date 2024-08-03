bcceba8360c8a8e69b352b585cf47c2e284d7ff5
Carter Walker <carter@goldtone.com>
2024-08-03 16:32:39 -0400

    SAUCE

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://sirmarshall.github.io',
-    base: '/',
-})
+  site: 'https://sirmarshall.github.io',
+  base: '/',
+  integrations: [react()]
+});
